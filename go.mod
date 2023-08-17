b86572059eb4e5510761a155b8edacf1d4f20c82
Richard McLaren <rickymclaren@gmail.com>
2023-08-17 21:37:40 +0100

    pipeline.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rickymclaren/goplayground
+
+go 1.21.0
---
+++ pipeline.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+// This is an example of syncronous channels forming a pipeline
+// The channels are syncronous because they are not buffered
+// Flow will ping-pong between the sender and receiver goroutines
+// Each step of the pipeline creates an output channel that will be used by a spun off goroutine
+// and then spins off the goroutine to process the input to the output channels
+
+func sliceToChannel(nums []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func double(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * 2
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	nums := []int{1, 3, 5, 7}
+
+	input := sliceToChannel(nums)
+	stage1 := double(input)
+	stage2 := square(stage1)
+
+	for n := range stage2 {
+		fmt.Println(n)
+	}
+
+	fmt.Println("Done")
+}
