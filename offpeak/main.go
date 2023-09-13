88a30738a7875a2e4caade820f7480461e31c5b3
Richard McLaren <rickymclaren@gmail.com>
2023-09-13 22:08:08 +0100

    UTC

--- offpeak/main.go
+++ offpeak/main.go
@@ -11,24 +11,44 @@ import (
 	"time"
 )
 
-var times = []string{
-	"23:30",
-	"00:00",
-	"00:30",
-	"01:00",
-	"01:30",
-	"02:00",
-	"02:30",
-	"03:00",
-	"03:30",
-	"04:00",
-	"04:30",
-	"05:00",
+var usage float64
+
+// ---------------------
+
+type consumptionData struct {
+	ts          time.Time
+	consumption float64
 }
 
-var gmt = times[2:11]
-var bst = times[0:9]
-var usage float64
+func newComsumptionData(line string) consumptionData {
+	cd := consumptionData{}
+	t, err := time.ParseInLocation("2006-01-02 15:04", line[1:17], time.UTC)
+	if err != nil {
+		panic(err)
+	}
+	cd.ts = t.Add(-30 * time.Minute) // move time from end of period to start
+
+	index := strings.Index(line, ",\"")
+	u := line[index+2 : len(line)-1]
+	f, err := strconv.ParseFloat(u, 32)
+	if err != nil {
+		fmt.Printf("Unable to parse %s\n", u)
+	} else {
+		cd.consumption = f
+	}
+
+	return cd
+}
+
+func (cd consumptionData) OffPeak() bool {
+	return cd.ts.Local().Hour() < 5
+}
+
+func (cd consumptionData) String() string {
+	return fmt.Sprintf("%s, %f", cd.ts.String(), cd.consumption)
+}
+
+// ---------------------
 
 func findByExtension(root, ext string) []string {
 	var a []string
@@ -42,15 +62,6 @@ func findByExtension(root, ext string) []string {
 		return nil
 	})
 	return a
-}
-
-func isOffpeak(times []string, line string) bool {
-	for _, s := range times {
-		if strings.Contains(line, s) {
-			return true
-		}
-	}
-	return false
 }
 
 func withinDates(startDate string, endDate string, line string) bool {
@@ -78,19 +89,12 @@ func addUsage(line string) {
 func main() {
 
 	if len(os.Args) != 3 {
-		fmt.Println("Usage main.go '2023-01-27 23:00' '2023-03-21 05:00'")
+		fmt.Println("Usage main.go '2023-01-27' '2023-03-21'")
 		return
 	}
 
-	startDate := os.Args[1]
-	endDate := os.Args[2]
-
-	var offpeak []string
-	if time.Now().IsDST() {
-		offpeak = bst
-	} else {
-		offpeak = gmt
-	}
+	// startDate := os.Args[1]
+	// endDate := os.Args[2]
 
 	files := findByExtension(".", ".csv")
 	if len(files) == 0 {
@@ -111,13 +115,17 @@ func main() {
 
 	fileScanner.Split(bufio.ScanLines)
 
+	fileScanner.Scan() // read header line
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		if isOffpeak(offpeak, line) && withinDates(startDate, endDate, line) {
-			fmt.Println(line)
+		cd := newComsumptionData(line)
+		if cd.OffPeak() {
+			fmt.Println(cd)
 			addUsage(line)
 		}
 	}
 	fmt.Printf("Usage: %f kWH\n", usage)
+
+	fmt.Println("The current time is " + time.Now().String())
 
 }
